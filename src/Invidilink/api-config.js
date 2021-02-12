8e5bff84e61d11fbbd781aad0fdcc4ac5f46f081
ktleary <kevin@ktleary.com>
2021-02-12 23:02:43 +0800

    add www

--- src/Invidilink/api-config.js
+++ src/Invidilink/api-config.js
@@ -2,7 +2,7 @@ let endpoint;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "invidi.link") {
+if (hostname === "invidi.link" || hostname === "www.invidi.link") {
   endpoint = "https://api.invidious.io/instances.json";
 } else if (hostname === "localhost") {
   endpoint = "http://localhost:5000/instance-data.json";
