db8e089eaae700df11e1eab620f74ff9db3bcc9e
djlooop <djlooop@stringtalk.org>
2021-06-21 06:12:15 -0400

    update api-config syntax

--- src/Invidilink/api-config.js
+++ src/Invidilink/api-config.js
@@ -1,11 +1,6 @@
-let endpoint;
-
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "invidi.link" || hostname === "www.invidi.link") {
-  endpoint = "https://api.invidious.io/instances.json";
-} else if (hostname === "localhost") {
-  endpoint = "http://localhost:5000/instance-data.json";
-}
-
-export const ENDPOINT = endpoint;
+export const ENDPOINT =
+  hostname === "invidi.link" || hostname === "www.invidi.link"
+    ? "https://api.invidious.io/instances.json"
+    : "http://localhost:5000/instance-data.json";
