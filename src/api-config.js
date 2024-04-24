5043d22961e1eb899cce5d4f1aee273d09909f40
Kevin <user1@matrix1.local>
2024-04-24 13:15:26 -0400

    update instance data example as instance-data.json

--- src/api-config.js
+++ src/api-config.js
@@ -3,4 +3,5 @@ const hostname = window && window.location && window.location.hostname;
 export const ENDPOINT =
   hostname === "invidi.link" || hostname === "www.invidi.link"
     ? "https://api.invidious.io/instances.json"
-    : "http://localhost:5000/instance-data.json";
+    : "https://api.invidious.io/instances.json";
+//"http://localhost:5000/instance-data.json";
